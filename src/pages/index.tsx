62a9fb7dd2a3fb335d0f11083c66e6ebf1100516
Sara Fernández Ramos <sararunda@hotmail.com>
2022-06-30 17:42:08 +0200

    add villagers image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import styles from './home.module.scss';
 import Image from 'next/image';
 import AcPic from '../../public/ac.png';
+import Minina from '../../public/minina.gif';
+import Link from 'next/link';
 const Home: NextPage = () => {
   return (
     <>
@@ -10,7 +12,21 @@ const Home: NextPage = () => {
           <Image width={0} height={0} src={AcPic} alt="foto camiseta"></Image>
         </div>
       </header>
-      <main>hola</main>
+      <main className={styles.main}>
+        <a href="">
+          <Link href="./villagers/">
+            <div className={styles.minina}>
+              <Image
+                src={Minina}
+                width={0}
+                height={0}
+                alt="Minina photo"
+              ></Image>
+              <p>Villagers</p>
+            </div>
+          </Link>
+        </a>
+      </main>
       <footer>hola</footer>
     </>
   );
