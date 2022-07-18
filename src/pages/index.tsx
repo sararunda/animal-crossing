c3186c041d0c77c321409aa0491d5b0869e20735
Sara Fernández Ramos <sararunda@hotmail.com>
2022-07-18 17:46:49 +0200

    change photos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import type { NextPage } from 'next';
 import styles from './home.module.scss';
 import Image from 'next/image';
 import AcPic from '../../public/ac.png';
-import Villagers from '../../public/minina.gif';
+import Fish from '../../public/fish.jpg';
+import Villagers from '../../public/villager.jpg';
 import Fossil from '../../public/fossil.jpg';
-import Butterfly from '../../public/butterfly.png';
+import Insect from '../../public/insect.jpg';
 import Link from 'next/link';
 const Home: NextPage = () => {
   return (
@@ -17,7 +18,8 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <Link href="">
           <div>
-            <Image src={Butterfly} width={0} height={0} alt=""></Image>
+            <Image src={Insect} width={0} height={0} alt=""></Image>
+            <p>Insects</p>
           </div>
         </Link>
         <Link href="">
@@ -30,6 +32,9 @@ const Home: NextPage = () => {
             height={0}
             alt="Villagers photo"
           ></Image>
+        </Link>
+        <Link href="">
+          <Image src={Fish} width={0} height={0} alt="Fossil photo"></Image>
         </Link>
       </main>
       <footer>hola</footer>
