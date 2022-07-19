91c0ebe7f471abf839da90846d9e309b807a4382
Sara Fernández Ramos <sararunda@hotmail.com>
2022-07-19 10:39:08 +0200

    change sort photos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import styles from './home.module.scss';
 import Image from 'next/image';
 import AcPic from '../../public/ac.png';
 import Fish from '../../public/fish.jpg';
-import Villagers from '../../public/villager.jpg';
 import Fossil from '../../public/fossil.jpg';
 import Insect from '../../public/insect.jpg';
+import Villagers from '../../public/villager.jpg';
 import Link from 'next/link';
 const Home: NextPage = () => {
   return (
@@ -16,25 +16,34 @@ const Home: NextPage = () => {
         </div>
       </header>
       <main className={styles.main}>
+        <Link href="./villagers/">
+          <div>
+            <Image src={Villagers} width={0} height={0} alt=""></Image>
+            <p>Villagers</p>
+          </div>
+        </Link>
         <Link href="">
           <div>
-            <Image src={Insect} width={0} height={0} alt=""></Image>
+            <Image src={Fossil} width={0} height={0} alt="Fossil photo"></Image>
+            <p>Fossils</p>
+          </div>
+        </Link>
+        <Link href="">
+          <div>
+            <Image
+              src={Insect}
+              width={0}
+              height={0}
+              alt="Villagers photo"
+            ></Image>
             <p>Insects</p>
           </div>
         </Link>
         <Link href="">
-          <Image src={Fossil} width={0} height={0} alt="Fossil photo"></Image>
-        </Link>
-        <Link href="./villagers/">
-          <Image
-            src={Villagers}
-            width={0}
-            height={0}
-            alt="Villagers photo"
-          ></Image>
-        </Link>
-        <Link href="">
-          <Image src={Fish} width={0} height={0} alt="Fossil photo"></Image>
+          <div>
+            <Image src={Fish} width={0} height={0} alt="Fossil photo"></Image>
+            <p>Fish</p>
+          </div>
         </Link>
       </main>
       <footer>hola</footer>
